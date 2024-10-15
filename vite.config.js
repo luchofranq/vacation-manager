81bce89b59cbac9ea1852a68365b65c7e8af8d7d
Luciano Franqueira <152804043+luchofranq@users.noreply.github.com>
2024-10-15 17:58:41 -0300

    a

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// Cambia 'tu-usuario' y 'tu-repositorio' por tu información real.
 export default defineConfig({
   plugins: [react()],
+  base: '/vacation-manager/', // Asegúrate de incluir la barra final
 });
